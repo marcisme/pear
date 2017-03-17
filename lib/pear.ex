9b7690441173435e7e2bcbc23d381f42c3c7899d
Marc Schwieterman <marc@marcisme.com>
2017-03-16 21:19:24 -0700

    Generate new project

---
+++ lib/pear.ex
@@ -0,0 +1,18 @@
+defmodule Pear do
+  @moduledoc """
+  Documentation for Pear.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pear.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pear_test.exs
@@ -0,0 +1,8 @@
+defmodule PearTest do
+  use ExUnit.Case
+  doctest Pear
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
