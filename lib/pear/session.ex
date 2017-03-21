497941d5bc6a914b633c3c3be363a3d1b1dc4069
Marc Schwieterman <marc@marcisme.com>
2017-03-20 20:35:34 -0700

    Improve Pear.Session API

--- lib/pear/session.ex
+++ lib/pear/session.ex
@@ -1,21 +1,31 @@
 defmodule Pear.Session do
-  def name(%{channel: channel, ts: ts}) do
+  def start_link(message) do
+    Agent.start_link(fn -> [] end, name: via_name(message))
+  end
+
+  def lookup(%{channel: channel, ts: ts}) do
+    Registry.lookup(__MODULE__, {channel, ts})
+  end
+
+  def user_ids(message) do
+    message
+    |> via_name
+    |> Agent.get(fn list -> list end)
+  end
+
+  def add(message, user_id) do
+    message
+    |> via_name
+    |> Agent.update(fn list -> [user_id | list] end)
+  end
+
+  def remove(message, user_id) do
+    message
+    |> via_name
+    |> Agent.update(&List.delete(&1, user_id))
+  end
+
+  defp via_name(%{channel: channel, ts: ts}) do
     {:via, Registry, {__MODULE__, {channel, ts}}}
-  end
-
-  def start_link(name) do
-    Agent.start_link(fn -> [] end, name: name)
-  end
-
-  def get(name) do
-    Agent.get(name, fn list -> list end)
-  end
-
-  def push(name, user_id) do
-    Agent.update(name, fn list -> [user_id | list] end)
-  end
-
-  def delete(name, user_id) do
-    Agent.update(name, &List.delete(&1, user_id))
   end
 end
--- test/pear/session_test.exs
+++ test/pear/session_test.exs
@@ -2,24 +2,26 @@ defmodule Pear.SessionTest do
   use ExUnit.Case, async: true
 
   setup do
-    name =
-      %{channel: "C1234567890", ts: "1234567890.123456"}
-      |> Pear.Session.name
-    {:ok, _} = Pear.Session.start_link(name)
-    {:ok, name: name}
+    message = %{channel: "C1234567890", ts: "1234567890.123456"}
+    {:ok, _} = Pear.Session.start_link(message)
+    {:ok, message: message}
   end
 
-  test "stores user ids", %{name: name} do
-    assert Pear.Session.get(name) == []
+  test "stores user ids", %{message: message} do
+    assert Pear.Session.user_ids(message) == []
 
-    Pear.Session.push(name, "U1234567890")
-    assert Pear.Session.get(name) == ["U1234567890"]
+    Pear.Session.add(message, "U1234567890")
+    assert Pear.Session.user_ids(message) == ["U1234567890"]
   end
 
-  test "deletes user ids", %{name: name} do
-    Pear.Session.push(name, "U1234567890")
+  test "deletes user ids", %{message: message} do
+    Pear.Session.add(message, "U1234567890")
 
-    Pear.Session.delete(name, "U1234567890")
-    assert Pear.Session.get(name) == []
+    Pear.Session.remove(message, "U1234567890")
+    assert Pear.Session.user_ids(message) == []
+  end
+
+  test "looks up session", %{message: message} do
+    assert Pear.Session.lookup(message)
   end
 end
