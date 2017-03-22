6181c7953c76e9792fea91870000a14ca913897a
Marc Schwieterman <marc@marcisme.com>
2017-03-21 21:50:25 -0700

    Ignore errors due to missing events

--- lib/pear/session.ex
+++ lib/pear/session.ex
@@ -1,31 +1,35 @@
 defmodule Pear.Session do
-  def start_link(message) do
-    Agent.start_link(fn -> [] end, name: via_name(message))
-  end
-
-  def lookup(%{channel: channel, ts: ts}) do
-    Registry.lookup(__MODULE__, {channel, ts})
+  def initialize(message) do
+    case Agent.start_link(fn -> [] end, name: via_name(message)) do
+      {:ok, pid} ->
+        {:ok, pid}
+      {:error, {:already_started, pid}} ->
+        Agent.update(pid, fn _ -> [] end)
+        {:ok, pid}
+    end
   end
 
   def user_ids(message) do
-    message
-    |> via_name
-    |> Agent.get(fn list -> list end)
+    with [{pid, _}] <- Registry.lookup(__MODULE__, key(message)) do
+      Agent.get(pid, fn list -> list end)
+    end
   end
 
   def add(message, user_id) do
-    message
-    |> via_name
-    |> Agent.update(fn list -> [user_id | list] end)
+    with [{pid, _}] <- Registry.lookup(__MODULE__, key(message)) do
+      Agent.update(pid, fn list -> [user_id | list] end)
+    end
   end
 
   def remove(message, user_id) do
-    message
-    |> via_name
-    |> Agent.update(&List.delete(&1, user_id))
+    with [{pid, _}] <- Registry.lookup(__MODULE__, key(message)) do
+      Agent.update(pid, &List.delete(&1, user_id))
+    end
   end
 
-  defp via_name(%{channel: channel, ts: ts}) do
-    {:via, Registry, {__MODULE__, {channel, ts}}}
+  defp via_name(%{channel: channel}) do
+    {:via, Registry, {__MODULE__, channel}}
   end
+
+  defp key(%{channel: channel}), do: channel
 end
--- test/pear/session_test.exs
+++ test/pear/session_test.exs
@@ -2,26 +2,40 @@ defmodule Pear.SessionTest do
   use ExUnit.Case, async: true
 
   setup do
-    message = %{channel: "C1234567890", ts: "1234567890.123456"}
-    {:ok, _} = Pear.Session.start_link(message)
+    message = %{channel: "1", ts: "2.3"}
+    {:ok, _} = Pear.Session.initialize(message)
     {:ok, message: message}
   end
 
-  test "stores user ids", %{message: message} do
-    assert Pear.Session.user_ids(message) == []
+  test "creates new sessions" do
+    assert {:ok, pid} = Pear.Session.initialize(%{channel: "1", ts: "2.3"})
+    assert is_pid(pid)
+    assert {:ok, pid} = Pear.Session.initialize(%{channel: "1", ts: "2.3"})
+    assert is_pid(pid)
+  end
 
+  test "reinitializes existing sessions" do
+    message = %{channel: "1", ts: "2.3"}
+    Pear.Session.initialize(message)
+    Pear.Session.add(message, "4")
+    Pear.Session.initialize(message)
+    assert Pear.Session.user_ids(message) == []
+  end
+
+  test "adds user ids", %{message: message} do
+    assert Pear.Session.user_ids(message) == []
     Pear.Session.add(message, "U1234567890")
     assert Pear.Session.user_ids(message) == ["U1234567890"]
   end
 
-  test "deletes user ids", %{message: message} do
+  test "removes user ids", %{message: message} do
     Pear.Session.add(message, "U1234567890")
-
     Pear.Session.remove(message, "U1234567890")
     assert Pear.Session.user_ids(message) == []
   end
 
-  test "looks up session", %{message: message} do
-    assert Pear.Session.lookup(message)
+  test "ignores unknown messages" do
+    Pear.Session.add(%{channel: "_", ts: "_"}, "_")
+    Pear.Session.remove(%{channel: "_", ts: "_"}, "_")
   end
 end
