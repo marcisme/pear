cc9612c2e8c6c231ca2ec1495bc2d79df82b1e31
Marc Schwieterman <marc@marcisme.com>
2017-03-25 16:16:03 -0700

    Suppress leading newline in log messages

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,9 @@ use Mix.Config
 
 import_config "secret.exs"
 
+config :logger, :console,
+  format: "$time $metadata[$level] $levelpad$message\n"
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
