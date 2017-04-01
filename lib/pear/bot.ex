830f3eea756b9762bb921b6b0d0d9e7e28e261a0
Marc Schwieterman <marc@marcisme.com>
2017-03-31 18:12:33 -0700

    Begin extracting commands

--- lib/pear/bot.ex
+++ lib/pear/bot.ex
@@ -2,6 +2,11 @@ defmodule Pear.Bot do
   use Slack
   require Logger
 
+  @commands [
+    Pear.Command.RandomPairCommand,
+    Pear.Command.RandomPairGoCommand
+  ]
+
   def handle_connect(slack, state) do
     Logger.debug "Connected as #{slack.me.name}"
     {:ok, state}
@@ -25,50 +30,77 @@ defmodule Pear.Bot do
 
   def handle_event(message = %{type: "message", subtype: "message_changed"}, slack, state) do
     log_message(message)
-    parse(message.message.text, slack)
-    |> command(message, slack)
+    # yeah...
+    Map.put(message, :text, message.message.text)
+    |> dispatch(slack)
     {:ok, state}
   end
 
   def handle_event(message = %{type: "message"}, slack, state) do
     log_message(message)
-    parse(message.text, slack)
-    |> command(message, slack)
+    dispatch(message, slack)
     {:ok, state}
   end
-
   def handle_event(_, _, state), do: {:ok, state}
 
-  defp parse(text, slack) do
-    cond do
-      !String.contains?(text, slack.me.id) -> nil
-      Regex.match?(~r/pair me/, text) -> :create_session
-      Regex.match?(~r/do it/, text) -> :generate_pairs
-      true -> nil
-    end
+  defp dispatch(message, slack) do
+    Enum.find(@commands, fn command ->
+      case command.execute(message) do
+        {service, action, args} ->
+          command(service, action, args, message, slack)
+        nil -> nil
+      end
+    end)
   end
-
-  defp command(:create_session, message, _slack) do
-    response = Slack.Web.Chat.post_message(message.channel, "Bring out your pears!")
-    Pear.Session.initialize(message)
-    Slack.Web.Reactions.add("pear", reaction_params(response))
-  end
-
-  defp command(:generate_pairs, message, slack) do
-    with user_ids when is_list(user_ids) <- Pear.Session.user_ids(message) do
-      Enum.map(user_ids, &Slack.Lookups.lookup_user_name(&1, slack))
-      |> Enum.join("\n")
-      |> send_message(message.channel, slack)
-    end
-  end
-
-  defp command(name, _message, _slack), do: name
 
   defp log_message(message) do
     Logger.debug "#{inspect(message)}"
+  end
+
+  defp command(:web, :post_and_react, [text, reaction], message, _slack) do
+    params =
+      Slack.Web.Chat.post_message(message.channel, text)
+      |> reaction_params()
+    Slack.Web.Reactions.add(reaction, params)
+  end
+
+  defp command(:rtm, :send, text, message, slack) do
+    replace_user_ids_with_names(text, slack)
+    |> send_message(message.channel, slack)
+  end
+
+  defp replace_user_ids_with_names(text, slack) do
+    Regex.replace(~r/({U[^}]+})/, text, fn _, user_id ->
+      Regex.replace(~r/[{}]/, user_id, "")
+      |> Slack.Lookups.lookup_user_name(slack)
+    end)
   end
 
   defp reaction_params(%{"channel" => channel, "ts" => timestamp}) do
     %{channel: channel, timestamp: timestamp}
   end
 end
+
+defmodule Pear.Command.RandomPairCommand do
+  def execute(message) do
+    if Regex.match?(~r/pair me/, message.text) do
+      Pear.Session.initialize(message)
+      {:web, :post_and_react, ["Bring out your pears!", "pear"]}
+    end
+  end
+end
+
+defmodule Pear.Command.RandomPairGoCommand do
+  def execute(message) do
+    if Regex.match?(~r/go/, message.text) do
+      with user_ids when is_list(user_ids) <- Pear.Session.user_ids(message) do
+        text =
+          user_ids
+          |> Enum.map(&("{#{&1}}"))
+          |> Enum.join("\n")
+        {:rtm, :send, text}
+      end
+    end
+  end
+end
+
