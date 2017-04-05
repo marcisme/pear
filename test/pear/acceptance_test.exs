33608b985714e8fff1d8f552a29802bdb6dedc57
Marc Schwieterman <marc@marcisme.com>
2017-04-04 21:43:55 -0700

    Convert `eventually` to a macro

--- test/pear/acceptance_test.exs
+++ test/pear/acceptance_test.exs
@@ -1,6 +1,7 @@
 defmodule Pear.AcceptanceTest do
   use ExUnit.Case, async: false
   alias Pear.TestBot
+  import Pear.Assertions
 
   @moduletag :capture_log
   @moduletag :acceptance
@@ -13,37 +14,17 @@ defmodule Pear.AcceptanceTest do
       {:ok, test_bot} = TestBot.start_link(@test_api_token)
 
       TestBot.send_test_message(test_bot, "@pear pair me", @test_channel)
-      eventually(fn ->
+      eventually do
         assert TestBot.has_message(test_bot, "Bring out your pears!")
-      end)
+      end
 
       TestBot.react(test_bot, "Bring out your pears!", "microscope")
 
       TestBot.send_test_message(test_bot, "@pear go", @test_channel)
-      eventually(fn ->
+      eventually do
         assert TestBot.has_message(test_bot, "Here are your pairs: @test1")
-      end)
-    end
-  end
-
-  defp eventually(timeout \\ 2_000, delay \\ 100, assertion) do
-    start = :os.system_time(:millisecond)
-    Enum.reduce_while(1..100, 0, fn _, acc ->
-      try do
-        assertion.()
-        {:halt, acc}
-      rescue
-        e ->
-          now = :os.system_time(:millisecond)
-          cond do
-            now - start < timeout ->
-              Process.sleep(delay)
-              {:cont, acc}
-            true ->
-              raise e
-          end
       end
-    end)
+    end
   end
 end
 
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,26 @@
 ExUnit.start()
 ExUnit.configure exclude: [:acceptance]
+
+defmodule Pear.Assertions do
+  defmacro eventually(timeout \\ 2_000, delay \\ 100, do: assertion) do
+    quote do
+      start = :os.system_time(:millisecond)
+      Enum.reduce_while(1..100, 0, fn _, acc ->
+        try do
+          unquote(assertion)
+          {:halt, acc}
+        rescue
+          e ->
+            now = :os.system_time(:millisecond)
+            cond do
+              now - start < unquote(timeout) ->
+                Process.sleep(unquote(delay))
+                {:cont, acc}
+              true ->
+                raise e
+            end
+        end
+      end)
+    end
+  end
+end
