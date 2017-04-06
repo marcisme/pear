b2ace96343edd7d779880689beff4e3b2885b815
Marc Schwieterman <marc@marcisme.com>
2017-04-05 23:01:09 -0700

    Split describe block

--- test/pear/command/random_pair_command_test.exs
+++ test/pear/command/random_pair_command_test.exs
@@ -8,19 +8,21 @@ defmodule Pear.Command.RandomPairCommandTest do
     end
   end
 
-  describe "RandomPairCommand.execute/2" do
-    test "returns a :post_and_react command when message contains 'pair me'" do
+  describe "RandomPairCommand.execute/2 when message contains 'pair me'" do
+    test "returns a :post_and_react command" do
       assert {:web, :post_and_react, ["1", "Bring out your pears!", "pear"]} =
         RandomPairCommand.execute(%{channel: "1", ts: "2.3", text: "a pair me b"}, nil)
     end
 
-    test "initializes a session when message contains 'pair me'" do
+    test "initializes a session" do
       message = %{channel: "1", ts: "2.3", text: "a pair me b"}
       RandomPairCommand.execute(message, nil)
       refute Pear.Session.user_ids(message) == :nosession
     end
+  end
 
-    test "continues execution when message does not contain 'pair me'" do
+  describe "RandomPairCommand.execute/2 when message does not contain 'pair me'" do
+    test "continues execution" do
       assert :cont = RandomPairCommand.execute(%{text: "nope"}, nil)
     end
   end
